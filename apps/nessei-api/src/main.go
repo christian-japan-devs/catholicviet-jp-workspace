2d06d0dd43f269a70ed762629d548a1f54c15d62
San Nguyen <vinhsannguyen91@gmail.com>
2020-10-30 09:50:43 +0900

    feat: add basic echo server

--- apps/nessei-api/src/main.go
+++ apps/nessei-api/src/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func Hello(name string) string {
 	result := "Hello " + name
@@ -9,4 +14,9 @@ func Hello(name string) string {
 
 func main() {
 	fmt.Println(Hello("nessei-api"))
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
